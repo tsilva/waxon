6ebd02d53917a580aa685412e48977dac784fd8b
Tiago Silva <eng.tiago.silva@gmail.com>
2026-06-17 14:03:17 +0100

    Prerender review route shell

--- app/(app)/review/page.tsx
+++ app/(app)/review/page.tsx
@@ -1,9 +1,6 @@
 import { ReviewHydrator } from "./ReviewHydrator";
 import { ReviewStaticView } from "./ReviewStaticView";
 
-export const runtime = "nodejs";
-export const dynamic = "force-dynamic";
-
 export default function ReviewPage() {
   return (
     <>
